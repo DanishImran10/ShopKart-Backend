98393c49a30950f49de35395eb81539fac36eec9
Danish Imran <danishimran015@gmail.com>
2026-09-02 19:21:18 +0500

    port shifted to host provided port

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ async function start() {
     await connectDB(process.env.DB_NAME)
 
     const app = express()
+    const PORT = process.env.PORT || 5000
 
     app.use(cors())
     app.use(express.json())
@@ -32,7 +33,7 @@ async function start() {
         });
     });
 
-    const server = app.listen(5000, () => {
+    const server = app.listen(PORT, () => {
         console.log("Server is running on port 5000")
     })
 }
